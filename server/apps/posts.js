ae830739c8c533392419019659debac3362f20b2
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2024-01-24 17:50:33 +0700

    finish exercise hackhour

--- server/apps/posts.js
+++ server/apps/posts.js
@@ -1,22 +1,50 @@
-import { Router } from "express";
+import { Router, query } from "express";
+import { connectingPool } from "../utils/db.js";
 
 const postRouter = Router();
 
 postRouter.get("/", async (req, res) => {
-  const status = req.query.status;
-  const keywords = req.query.keywords;
-  const page = req.query.page;
+  const status = req.query.status || "";
+  const keywords = req.query.keywords || "";
+  const page = req.query.page || 1;
+
+  const PAGE_SIZE = 5;
+  const offset = (page - 1) * PAGE_SIZE;
+
+  let query = "";
+  let values = [];
+
+  if (status && keywords) {
+    query = `select * from posts where status=$1 and title ilike $2 limit $3 offset $4`;
+    values = [status, keywords, PAGE_SIZE, offset];
+  } else if (keywords) {
+    query = `select * from posts where title ilike $1 limit $2 offset $3`;
+    values = [keywords, PAGE_SIZE, offset];
+  } else if (status) {
+    query = "select * from posts where status=$1 limit $2 offset $3";
+    values = [status, PAGE_SIZE, offset];
+  } else {
+    query = "select * from posts order by post_id asc limit $1 offset $2";
+    values = [PAGE_SIZE, offset];
+  }
+
+  const result = await connectingPool.query(query, values);
 
   return res.json({
-    data: [],
+    data: result.rows,
   });
 });
 
 postRouter.get("/:id", async (req, res) => {
   const postId = req.params.id;
 
+  const result = await connectingPool.query(
+    "select * from posts where post_id=$1",
+    [postId]
+  );
+
   return res.json({
-    data: {},
+    data: result.rows[0],
   });
 });
 
@@ -28,6 +56,18 @@ postRouter.post("/", async (req, res) => {
     updated_at: new Date(),
     published_at: hasPublished ? new Date() : null,
   };
+
+  await connectingPool.query(
+    "insert into posts (title, content, status, created_at, updated_at, published_at) values ($1, $2, $3, $4, $5, $6)",
+    [
+      newPost.title,
+      newPost.content,
+      newPost.status,
+      newPost.created_at,
+      newPost.updated_at,
+      newPost.published_at,
+    ]
+  );
 
   return res.json({
     message: "Post has been created.",
@@ -44,6 +84,18 @@ postRouter.put("/:id", async (req, res) => {
   };
   const postId = req.params.id;
 
+  await connectingPool.query(
+    "update posts set title=$1, content=$2, status=$3, updated_at=$4, published_at=$5 where post_id=$6",
+    [
+      updatedPost.title,
+      updatedPost.content,
+      updatedPost.status,
+      updatedPost.updated_at,
+      updatedPost.published_at,
+      postId,
+    ]
+  );
+
   return res.json({
     message: `Post ${postId} has been updated.`,
   });
@@ -51,6 +103,8 @@ postRouter.put("/:id", async (req, res) => {
 
 postRouter.delete("/:id", async (req, res) => {
   const postId = req.params.id;
+
+  await connectingPool.query("delete from posts where post_id=$1", [postId]);
 
   return res.json({
     message: `Post ${postId} has been deleted.`,
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,9 @@
 // Creating PostgreSQL Client here
+import pg from "pg";
+const { Pool } = pg;
+
+const connectingPool = new Pool({
+  connectionString: "postgresql://admin:root@localhost:5432/practic-postgres",
+});
+
+export { connectingPool };
